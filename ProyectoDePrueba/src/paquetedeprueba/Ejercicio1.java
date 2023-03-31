936904efbc627bc4dc45497b7acf3b285af6998a
Rosana Nuñez <rotenu_1975@hotmail.com>
2023-03-31 01:27:27 -0300

    Proyecto creado y archivos eliminados

--- ProyectoDePrueba/src/paquetedeprueba/Ejercicio1.java
+++ ProyectoDePrueba/src/paquetedeprueba/Ejercicio1.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package paquetedeprueba;
 
-/**
- *
- * @author roten
- */
+import java.util.Scanner;
+
+/*Escribir un programa que pida dos numeros enteros por teclado y calculelasuma de los dos.
+El programa debera despues mostrar el resultado de la suma */
+
 public class Ejercicio1 {
     
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese el valor de dos numeros");
+        int num1 = leer.nextInt();
+        int num2 = leer.nextInt();
+        int suma = num1 + num2;
+        System.out.println("El resultado de la suma es : " + suma);
+        //Definir numero como entero numero = 5;
+        //int numero = 5;
+        //TRES TERMINOS QUE UTILIZAMOS:
+        //DECLARAR
+        //INICIALIZAR
+        //ASIGNAR  
+        //double numeroDecimal = 5.22;
+        //boolean booleano = true;
+       //String nombre = "Rosana";
+        // Ejemplo para imprimir en pantalla System.out.println("Hola Bienvenido al programa" + numero);
+        //numero++ es incrementar, es igual que numero = numero + 1;
+       
+        
+    }
 }
